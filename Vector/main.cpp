bd4505676022b91483be50a52127d1ecb643ebc7
Alec Halici <alechalici@gmail.com>
2025-04-27 00:30:49 -0700

    - Added Utils.cpp and Utils.h - Utility functions - Added plane (red rectangle)

--- Vector/main.cpp
+++ Vector/main.cpp
@@ -4,21 +4,137 @@
 #include <string>
 #include <iostream>
 #include <fstream>
-
+#include <cmath>
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp> // glm::value_ptr
+#include <glm/gtc/matrix_transform.hpp> // glm::translate, glm::rotate, glm::scale, glm::perspective
+#include "Utils.h"
 
 using namespace std;
 
+#define numVAOs 1
+#define numVBOs 1
+
+float toRadians(float degrees) { return (degrees * 2.0f * 3.14159f) / 360.0f; }
+
+GLuint renderingProgram;
+GLuint vao[numVAOs];
+GLuint vbo[numVBOs];
+
+// Display variables
+GLuint mvLoc, pLoc;
+int width, height;
+float aspect;
+glm::mat4 mMat, vMat, pMat, mvMat;
+
+// Camera
+glm::vec3 cameraLoc;
+
+// Object Locations
+float planeLocX, planeLocY, planeLocZ;
+
+// Object number of vertices
+unsigned int planeNumVertices;
+
+
+void setupVertices()
+{
+	float planeVertexPositions[18] =  
+	{
+	   -1.0f, 0.0f, 0.0f,
+		1.0f, 0.0f, 1.0f,
+	   -1.0f, 0.0f, 1.0f,
+
+	   -1.0f, 0.0f, 0.0f,
+	    1.0f, 0.0f, 0.0f,
+		1.0f, 0.0f, 1.0f
+	};
+
+	planeNumVertices = 6;
+
+	// Init VAO and VBO 
+	glGenVertexArrays(1, vao);
+	glBindVertexArray(vao[0]);
+	glGenBuffers(numVBOs, vbo);
+
+	glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(planeVertexPositions), planeVertexPositions, GL_STATIC_DRAW);
+}
+
 void init(GLFWwindow* window)
 {
-
+	renderingProgram = Utils::createShaderProgram("vertShader.glsl", "fragShader.glsl");
+	cameraLoc = glm::vec3(0.0f, 0.0f, 5.0f);
+	planeLocX = 0.0f; planeLocY = 0.0f; planeLocZ = 0.0f;
+	setupVertices();
 }
 
 void display(GLFWwindow* window, double currentTime)
 {
+	glClear(GL_DEPTH_BUFFER_BIT);
+	glClear(GL_COLOR_BUFFER_BIT);
 
+	glUseProgram(renderingProgram);
+
+	mvLoc = glGetUniformLocation(renderingProgram, "mv_matrix");
+	pLoc = glGetUniformLocation(renderingProgram, "p_matrix");
+
+	// Set up frustum and perspective matrix
+	glfwGetFramebufferSize(window, &width, &height);
+	aspect = (float)width / (float)height;
+	pMat = glm::perspective(1.0472f, aspect, 0.1f, 1000.0f); // 1.0472 is about 60 degrees in radians
+
+	// View, Model, and Model-View matrices
+	vMat = glm::translate(glm::mat4(1.0f), -cameraLoc);
+	mMat = glm::translate(glm::mat4(1.0f), glm::vec3(planeLocX, planeLocY, planeLocZ));
+	mMat = glm::rotate(mMat, toRadians(20.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+	mMat = glm::scale(mMat, glm::vec3(2.0f, 0.0f, 2.0f));
+	mvMat = vMat * mMat;
+
+	// Model-View matrix pointer sent to uniforms
+	glUniformMatrix4fv(mvLoc, 1, GL_FALSE, glm::value_ptr(mvMat));
+	glUniformMatrix4fv(pLoc, 1, GL_FALSE, glm::value_ptr(pMat));
+
+	// Bind vertex attribute to vbo[0] values and enable vertex attribute
+	glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+	glVertexAttribPointer(0, 3, GL_FLOAT, false, 0, 0);
+	glEnableVertexAttribArray(0);
+
+	glEnable(GL_DEPTH_TEST);
+	glDepthFunc(GL_LEQUAL);
+
+	glDrawArrays(GL_TRIANGLES, 0, planeNumVertices);
 }
 
 int main(void)
 {
+	// GLFW init
+	if (!glfwInit()) { exit(EXIT_FAILURE); }
 
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+
+	// Full Screen Monitor Options
+	/*GLFWmonitor* primaryMonitor = glfwGetPrimaryMonitor();
+	const GLFWvidmode* mode = glfwGetVideoMode(primaryMonitor);
+	GLFWwindow* window = glfwCreateWindow(mode->width, mode->height, "Vector - Puzzle Game", primaryMonitor, NULL);*/
+
+	GLFWwindow* window = glfwCreateWindow(1200, 800, "Vector - Puzzle Game", NULL, NULL);
+	glfwMakeContextCurrent(window);
+
+	if (glewInit() != GLEW_OK) { exit(EXIT_FAILURE); }
+	glfwSwapInterval(1);
+
+	init(window);
+
+	while (!glfwWindowShouldClose(window)) 
+	{
+		display(window, glfwGetTime());
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
+	exit(EXIT_SUCCESS);
 }

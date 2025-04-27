1615e2c9b820676175850e047ee93cc28ed2061f
alech <alech@ALECPC>
2025-04-26 19:36:22 -0700

    Add project files.

---
+++ Vector/main.cpp
@@ -0,0 +1,24 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <SOIL2/SOIL2.h>
+#include <string>
+#include <iostream>
+#include <fstream>
+
+
+using namespace std;
+
+void init(GLFWwindow* window)
+{
+
+}
+
+void display(GLFWwindow* window, double currentTime)
+{
+
+}
+
+int main(void)
+{
+
+}
